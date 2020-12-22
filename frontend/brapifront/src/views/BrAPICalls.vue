ad82569101c0499ba36dbf7350c088a7a91b7217
edarnige <edarnige@nsat-edarnige.localdomain>
2020-12-22 06:45:12 +0100

    Add list to BrAPI test server v2 crop call

--- frontend/brapifront/src/views/BrAPICalls.vue
+++ frontend/brapifront/src/views/BrAPICalls.vue
@@ -1,10 +1,24 @@
 <template>
-  <div>
+  <div class="container">
 
-    <h1>BrAPI calls</h1>
+    <h1>BrAPI test server v2 call</h1>
 
-    <div class="">
-      {{info}}
+    <div class="mt-5">
+      <h3>Raw data</h3>
+      <div class="">
+        {{info}}
+      </div>
+    </div>
+
+    <div class="mt-5">
+      <h3>Crop names</h3>
+
+      <div>
+        <ul>
+          <li v-for="c in c_data" v-bind:key="c">{{c}}</li>
+        </ul>
+
+      </div>
     </div>
 
   </div>
@@ -17,14 +31,15 @@ export default {
   name: 'brapicalls',
   data() {
     return{
-      info: null
+      info: null,
+      c_data: null
     }
   },
   mounted(){
     axios
       .get('https://test-server.brapi.org/brapi/v2/commoncropnames')
-      .then(response => (this.info = response))
-
+      .then(response => (this.info = response,
+          this.c_data = response.data.result.data))
   }
 }
 </script>
